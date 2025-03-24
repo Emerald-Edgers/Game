51d1d17c816cb8a664cc21bab484f6a593b6f00f
Victor Kahl Petersen <victorkahlpetersen@gmail.com>
2025-03-24 17:42:19 +0100

    Linting

--- CommonEnemy/src/main/java/dk/ee/zg/common/enemy/interfaces/IEnemy.java
+++ CommonEnemy/src/main/java/dk/ee/zg/common/enemy/interfaces/IEnemy.java
@@ -1,5 +1,10 @@
 package dk.ee.zg.common.enemy.interfaces;
 
 public interface IEnemy {
+    /**
+     * Should extend this method by implementing an attack method.
+     * Is purely used within its own implementation,
+     * so can be left empty if needed.
+     */
     void attack();
 }
