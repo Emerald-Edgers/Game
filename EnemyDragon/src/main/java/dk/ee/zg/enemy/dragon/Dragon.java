e5aced3e9a4c56e6697eced11875a2252b5fc2f9
Victor Bøgesvang Pedersen <73688869+strikZie@users.noreply.github.com>
2025-04-06 16:41:39 +0200

    Update Dragon.java

--- EnemyDragon/src/main/java/dk/ee/zg/enemy/dragon/Dragon.java
+++ EnemyDragon/src/main/java/dk/ee/zg/enemy/dragon/Dragon.java
@@ -7,13 +7,13 @@ import dk.ee.zg.common.map.data.EntityType;
 public class Dragon extends Enemy {
 
     /**
-     * Main constructor for the skeleton.
-     * @param attackDamage The amount of attack damage the skeleton should have.
-     * @param attackSpeed The amount of attack speed the skeleton should have.
-     * @param moveSpeed The amount of move speed the skeleton should have.
-     * @param hitpoints The amount of hp the skeleton should have.
-     * @param defense The amount of defense the skeleton should have.
-     * @param cost The cost skeleton should have.
+     * Main constructor for the dragon.
+     * @param attackDamage The amount of attack damage the dragon should have.
+     * @param attackSpeed The amount of attack speed the dragon should have.
+     * @param moveSpeed The amount of move speed the dragon should have.
+     * @param hitpoints The amount of hp the dragon should have.
+     * @param defense The amount of defense the dragon should have.
+     * @param cost The cost dragon should have.
      * @param spawnPoint This is a set of x,y coordinates to determine where it
      *                   should be spawned
      */
